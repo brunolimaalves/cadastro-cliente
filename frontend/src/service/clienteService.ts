9356ee20f3c1c3a40e403ccbb484312c9ea366ab
Bruno Lima Alves <brunolimaalves85@gmail.com>
2025-02-19 16:01:38 -0300

    fix: removendo dependencias desnecessarias

--- frontend/src/service/clienteService.ts
+++ frontend/src/service/clienteService.ts
@@ -16,19 +16,19 @@ export const useClienteService = () => {
     
     const list = useCallback(async ({ params }: any) => {
         return api.get(path, params );
-    }, [api]);
+    }, []);
 
     const post = useCallback(async ( data: any) => {
         return api.post(`${path}`, { ...data });
-    }, [api]);
+    }, []);
 
     const update = useCallback(async ( data: any, id: number) => {
         return api.put(`${path}/${id}`, { ...data });
-    }, [api]);
+    }, []);
 
     const deleteById = useCallback(async ( id: number) => {
         return api.delete(`${path}/${id}`);
-    }, [api]);
+    }, []);
 
     return {
         list,post,update, deleteById
